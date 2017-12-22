b85e90c92b110c3af2d57a7e44fbbaa4a7831ebe
daniellp20 <dany15-paradas@hotmail.com>
2017-12-22 18:43:27 +0100

    Arreglo de responder en auditor

---
+++ Entrega/Item 2/Acme-Explorer/src/main/java/domain/Tag.java
@@ -0,0 +1,76 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Tag extends DomainEntity {
+
+	// Attributes -------------------------------------------------------------
+	private String	name;
+
+
+	@NotBlank
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+
+	// Relationships ----------------------------------------------------------
+	private Collection<Value>	values;
+
+
+	@Valid
+	@OneToMany(mappedBy = "tag", cascade = CascadeType.REMOVE)
+	public Collection<Value> getValues() {
+		return this.values;
+	}
+
+	public void setValues(Collection<Value> values) {
+		this.values = values;
+	}
+
+	/*
+	 * private Collection<Value> values;
+	 * private Collection<Trip> trips;
+	 * 
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToMany
+	 * public Collection<Value> getValues() {
+	 * return this.values;
+	 * }
+	 * 
+	 * public void setValues(Collection<Value> values) {
+	 * this.values = values;
+	 * }
+	 * 
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToMany(mappedBy = "tags")
+	 * public Collection<Trip> getTrips() {
+	 * return this.trips;
+	 * }
+	 * 
+	 * public void setTrips(Collection<Trip> trips) {
+	 * this.trips = trips;
+	 * }
+	 */
+
+}
